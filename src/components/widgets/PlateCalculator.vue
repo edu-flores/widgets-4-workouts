a8aba2627a9670668f9b9ac0c9731b6016b138dd
Eduardo Flores <eduardo.floresramones@gmail.com>
2023-01-13 14:18:51 -0600

    position barbell in plate calculator

--- src/components/widgets/PlateCalculator.vue
+++ src/components/widgets/PlateCalculator.vue
@@ -9,9 +9,13 @@ import MenuIcon from '../icons/IconMenu.vue';
     </div>
     <h6><b>Plate Calculator</b></h6>
     <div class="container-fluis">
-      <div class="row">
+      <div class="row align-items-center justify-content-between">
         <!-- Visual Representation -->
-        <div class="col-12 col-sm-6 col-md-12 col-lg-6">Barbell</div>
+        <div id="barbell" class="col-12 col-sm-6 col-md-12 col-lg-6">
+          <div id="inner-bar"></div>
+          <div id="separation"></div>
+          <div id="outer-bar"></div>
+        </div>
         <!-- Text -->
         <div class="col-12 col-sm-6 col-md-12 col-lg-6 text-center">
           <p class="mb-0"><b>185 lbs</b></p>
@@ -28,6 +32,37 @@ import MenuIcon from '../icons/IconMenu.vue';
 .menu {
   margin-top: -1.5rem;
   margin-right: -1rem;
+}
+
+#barbell {
+  position: relative;
+  margin-left: -2rem;
+}
+
+#inner-bar {
+  background-color: $light;
+  width: 100%;
+  height: 0.4rem;
+}
+
+#separation {
+  position: relative;
+  top: -0.8rem;
+  left: 3rem;
+  background-color: $light;
+  width: 0.5rem;
+  height: 1.2rem;
+  border-radius: 3px;
+}
+
+#outer-bar {
+  position: relative;
+  top: -1.7rem;
+  left: 3rem;
+  background-color: $light;
+  width: 7rem;
+  height: 0.6rem;
+  border-radius: 3px;
 }
 
 p {
