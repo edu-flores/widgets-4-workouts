552e542a4f17fdb2bcffc58c9af64a06252b7c1f
Eduardo Flores <eduardo.floresramones@gmail.com>
2023-01-19 17:38:01 -0600

    set up goal component

--- src/components/widgets/Goals.vue
+++ src/components/widgets/Goals.vue
@@ -1,6 +1,9 @@
 <script setup>
 // Icons
 import PlusIcon from '../icons/IconPlus.vue';
+
+// Components
+import Goal from '../Goal.vue';
 </script>
 
 <template>
@@ -13,20 +16,12 @@ import PlusIcon from '../icons/IconPlus.vue';
     <div class="container-fluid">
       <div class="row">
         <!-- Progress Bars -->
-        <div class="col-12 col-sm-6 col-lg-12">
-          <p class="goal"><b>Total Volume</b></p>
-          <progress class="w-100" value="32" max="100"></progress>
-          <div class="text-end">
-            <span>2,134 / 3,000 lbs</span>
-          </div>
-        </div>
-        <div class="col-12 col-sm-6 col-lg-12">
-          <p class="goal"><b>Repetitions</b></p>
-          <progress class="w-100" value="62" max="100"></progress>
-          <div class="text-end">
-            <span>123 / 180 reps</span>
-          </div>
-        </div>
+        <Goal
+          name="Total Volume"
+          :progress="1000"
+          :limit="2000"
+          units="lbs"
+        />
       </div>
     </div>
     <!-- Edit Goals -->
@@ -40,18 +35,12 @@ import PlusIcon from '../icons/IconPlus.vue';
 <style lang="scss" scoped>
 @import '../../assets/main.scss';
 
-p {
-  color: $darker;
-  font-size: medium;
-  margin-bottom: -0.2rem;
+.add {
+  @include flexbox(column, center, center);
+  gap: 0.5rem;
 }
 
 span {
   font-size: small;
-}
-
-.add {
-  @include flexbox(column, center, center);
-  gap: 0.5rem;
 }
 </style>
