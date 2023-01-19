b0e958613ef0727091c087ab69d186616c0d0154
Eduardo Flores <eduardo.floresramones@gmail.com>
2023-01-18 22:25:40 -0600

    enter personal weight

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,23 @@
 <script setup>
+// Vue API
+import { ref } from 'vue';
+
 // Icons
 import ScaleIcon from './icons/IconScale.vue';
 import CalendarIcon from './icons/IconCalendar.vue';
 import MoonIcon from './icons/IconMoon.vue';
 
-// Data
+// Personal weight
+const weight = 70.5;
+const input = ref(null);
+const resize = () => {
+  if (String(input.value.value).includes('.'))
+    input.value.style.width = (input.value.value.length - 0.7) + 'ch';
+  else
+    input.value.style.width = (input.value.value.length) + 'ch';
+}
+
+// Today's date
 const today = new Date();
 const days = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -16,7 +29,7 @@ const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
       <!-- Personal Weight -->
       <div class="order-2 col-6 col-md-3 text-start">
         <ScaleIcon class="me-2 mb-1" />
-        <span>75.2 kg</span>
+        <input ref="input" @input="resize" type="number" min="0.0" max="200.0" step="0.1" v-model="weight" /> kgs
       </div>
       <!-- Date -->
       <div class="order-1 order-md-2 col-12 col-md-6 text-center">
@@ -44,6 +57,22 @@ header {
   box-shadow: rgba(33, 35, 38, 0.1) 0px 10px 10px -10px;
 }
 
+input {
+  all: unset;
+  width: 3.3ch;
+}
+
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+input[type=number] {
+  -moz-appearance: textfield;
+  appearance: textfield;
+}
+
 .row {
   min-height: inherit;
 }
@@ -51,4 +80,5 @@ header {
 b {
   color: $darker;
 }
+
 </style>
